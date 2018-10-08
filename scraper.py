55cb10e2fa630bc4ba8708258b068c54b2dc212e
WayneLi7 <p.li@berkeley.edu>
2018-10-08 13:42:07 -0700

    scraper done

---
+++ scraper.py
@@ -0,0 +1,25 @@
+import urllib2
+from bs4 import BeautifulSoup
+import sys
+
+def get_same_domain_links(url):
+	response = urllib2.urlopen(url)
+	html = response.read()
+	soup = BeautifulSoup(html, 'html.parser')
+	##find all <a> tag
+	a_tags = soup.find_all('a')
+	link_count = 0
+	##get hostname
+	search_domain = urllib2.urlparse.urlparse(url).hostname
+	for a_tag in a_tags:
+		link = a_tag.get('href')
+		##if it is a internal link or it is a hyperlink points to same host
+		if search_domain in link or ("http" not in link and "/" in link):
+			print(a_tag.get('href'))
+			link_count += 1
+	return (len(html), link_count)
+
+if __name__ == "__main__":
+    print(get_same_domain_links(sys.argv[1]))
+
+
